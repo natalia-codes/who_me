33b6e123cbfa19bddafeaea4389fca88ce3c0691
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2023-02-15 15:56:21 -0800

    sign up

--- website/main/views.py
+++ website/main/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .forms import RegisterFrom
+
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'main/home.html')
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterFrom(request.POST)
+    else: 
+        form = RegisterFrom()
+    
+    return render(request, 'registration/sign_up.html', {"form": form})
