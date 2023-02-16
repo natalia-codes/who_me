0752344a1564f15d4c366fac7a9abd11e0146ce2
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2023-02-15 16:32:46 -0800

    authentication for home

--- website/main/views.py
+++ website/main/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterFrom
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
 
 
 # Create your views here.
 
-
+@login_required(login_url="/login")
 def home(request):
     return render(request, 'main/home.html')
 
@@ -12,6 +14,10 @@ def home(request):
 def sign_up(request):
     if request.method == 'POST':
         form = RegisterFrom(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/home')
     else: 
         form = RegisterFrom()
     
