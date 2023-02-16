0ff14b8f7d4a0f9cc8a362b3417a13cf9a38d12a
Natalia Palmatier <87651358+natalia-codes@users.noreply.github.com>
2023-02-16 14:49:19 -0800

    fixed permissions

--- website/main/apps.py
+++ website/main/apps.py
@@ -2,20 +2,27 @@ from django.apps import AppConfig
 from django.conf import settings
 
 
+
+
 class MainConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "main"
 
 # ensure that when a user is added, they are put into the default group automatically
     def ready(self):
-        from django.contrib.auth.models import Group
+        from django.contrib.auth.models import User, Group
         from django.db.models.signals import post_save
 
         def add_to_default_group(sender, **kwargs):
             user = kwargs["instance"]
-
-            if kwargs["created"]:
-                group, ok = Group.objects.get_or_create(name="default")
+            if kwargs['created']:
+                group, ok = Group.objects.get_or_create(name="add_post")
                 group.user_set.add(user)
 
-        post_save.connect(add_to_default_group, sender=settings.AUTH_USER_MODEL)
+        post_save.connect(add_to_default_group,
+                          sender= settings.AUTH_USER_MODEL)
+
+            # if kwargs.get('created', False):
+            #     user = kwargs.get('instance')
+            #     group = Group.objects.get(name='default')
+            #     user.groups.add(group)
